723f2fb7c8c1c5a6fc63be33431f171b09502c88
danlin0226 <danlin0226@gmail.com>
2022-11-09 23:11:08 -0800

    created index for components

--- src/pages/videos-page/VideosPage.jsx
+++ src/pages/videos-page/VideosPage.jsx
@@ -2,52 +2,47 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 //importing utils
-import { getVideos, getVideoDetails } from "../../utils/utils.jsx";
+import { getVideos, getVideoDetails } from "../../utils/axiosUtils.jsx";
 import { dynamicTime } from "../../utils/dateUtils.jsx";
 
 //importing components
-import MainVideo from "../../components/main-video/MainVideo";
-import MainVideoTitle from "../../components/main-video-title/MainVideoTitle";
-import MainVideoStats from "../../components/main-video-stats/MainVideoStats";
-import MainVideoDescription from "../../components/main-video-description/MainVideoDescription";
-import CommentForm from "../../components/comment-form/CommentForm";
-import CommentList from "../../components/comment-list/CommentList";
-import NextVideoList from "../../components/next-video-list/NextVideoList";
+import {
+  MainVideo,
+  MainVideoTitle,
+  MainVideoStats,
+  MainVideoDescription,
+  CommentForm,
+  CommentList,
+  NextVideoList,
+} from "../../components";
 
 export default function VideosPage() {
-  //define states
+  // define states
   const [videos, setVideos] = useState([]);
-  console.log("1 videos", videos);
   const [videoDetails, setVideoDetails] = useState({});
-  console.log("2 videosDetails", videoDetails);
-
-  const defaultVideoID = videos.length > 0 ? videos[0].id : false;
-  console.log("3 default video id", defaultVideoID);
 
   const { videoID } = useParams();
-  console.log("4 params:", videoID);
+
+  const defaultVideoID = videos.length > 0 ? videos[0].id : false;
   const displayedVideoID = videoID || defaultVideoID;
 
   const displayedVideos = videos.filter(
     (video) => video.id !== displayedVideoID
   );
-  console.log("5 displayed videos:", displayedVideos);
 
-  //effect hook for setting videos
+  // effect hook for initial videos api call
   useEffect(() => {
     const fetchVideosData = async () => {
       const request = await getVideos();
-      console.log("GET videos result:", request);
-      setVideos(request);
+      setVideos(request); //sets videos as an array of all videos from api
     };
     fetchVideosData();
   }, []);
 
-  //effect hook for setting video details
+  //effect hook for video details api call
   useEffect(() => {
     const fetchDetailsData = async () => {
       const request = await getVideoDetails(displayedVideoID);
-      console.log("GET videoDetails", request);
       setVideoDetails(request);
     };
     displayedVideoID && fetchDetailsData();
